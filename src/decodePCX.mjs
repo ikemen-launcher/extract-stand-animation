f21c738997b4ddb3246c1e8018599f7772f2ec7a
neolao <contact@neolao.com>
2024-02-18 08:25:13 +0100

    up

--- src/decodePCX.mjs
+++ src/decodePCX.mjs
@@ -1,111 +1,9 @@
-import convertPaletteRGBtoRGBA from './convertPaletteRGBtoRGBA.mjs';
+import convertPaletteRGBtoRGBA from "./convertPaletteRGBtoRGBA.mjs";
 
-export default function decodePCX(buffer, width, height, palette) {
-  let offset = 0;
-
-  const id = buffer.readUInt8(offset);
-  //console.log(`  PCX id: ${id}`);
-  offset += 1;
-  
-  const version = buffer.readUInt8(offset);
-  //console.log(`  PCX version: ${version}`);
-  offset += 1;
-  
-  const encoding = buffer.readUInt8(offset);
-  //console.log(`  PCX encoding: ${encoding}`);
-  offset += 1;
-  
-  const bitPerPixel = buffer.readUInt8(offset);
-  //console.log(`  PCX bitPerPixel: ${bitPerPixel}`);
-  offset += 1;
-  
-  const x = buffer.readUInt16LE(offset);
-  //console.log(`  PCX x: ${x}`);
-  offset += 2;
-  
-  const y = buffer.readUInt16LE(offset);
-  //console.log(`  PCX y: ${y}`);
-  offset += 2;
-  
-  let pcxWidth = buffer.readUInt16LE(offset);
-  //console.log(`  PCX width: ${pcxWidth}`);
-  offset += 2;
-  
-  let pcxHeight = buffer.readUInt16LE(offset);
-  //console.log(`  PCX height: ${pcxHeight}`);
-  offset += 2;
-  
-  const hres = buffer.readUInt16LE(offset);
-  //console.log(`  PCX hres: ${hres}`);
-  offset += 2;
-  
-  const vres = buffer.readUInt16LE(offset);
-  //console.log(`  PCX vres: ${vres}`);
-  offset += 2;
-  
-  const colorMap = []; // 16 colors rgb
-  for (let i = 0; i < 16; i++) {
-    var c = [];
-    c[0] = buffer.readUInt8(offset);
-    offset += 1;
-    c[1] = buffer.readUInt8(offset);
-    offset += 1;
-    c[2] = buffer.readUInt8(offset);
-    offset += 1;
-    colorMap.push(c);
-  }
-
-  const reserved = buffer.readUInt8(offset);
-  //console.log(`  PCX reserved: ${reserved}`);
-  offset += 1;
-  
-  const nbPlanes = buffer.readUInt8(offset);
-  //console.log(`  PCX nbPlanes: ${nbPlanes}`);
-  offset += 1;
-  
-  const bpl = buffer.readUInt16LE(offset);
-  //console.log(`  PCX bpl: ${bpl}`);
-  offset += 2;
-  
-  const paletteInfo = buffer.readUInt16LE(offset);
-  //console.log(`  PCX paletteInfo: ${paletteInfo}`);
-  offset += 2;
-
+function implementation1(buffer, pcxWidth, pcxHeight, palette) {
   const out = Buffer.alloc(pcxWidth * pcxHeight * 4);
 
-  offset = 128;
-  /*
-  const rle = buffer.subarray(offset);
-    let i = 0, j = 0, k = 0, w = pcxWidth;
-    while (j < out.length) {
-      let n = 1;
-      let d = rle[i];
-        if (i < rle.length - 1) {
-            i++;
-        }
-        if (d >= 0xc0) {
-            n = d & 0x3f;
-            d = rle[i];
-            if (i < rle.length - 1) {
-                i++;
-            }
-        }
-        while (n > 0) {
-            if (k < w && j < out.length) {
-                out[j] = d;
-                j++;
-            }
-          k++;
-            if (k === pcxHeight) {
-                k = 0;
-                n = 1;
-            }
-            n--;
-        }
-    }
-    //*/
-
-  ///*
+  let offset = 0;
   let tempX = 0;
   let tempY = 0;
   while (tempY < pcxHeight) {
@@ -139,7 +37,136 @@ export default function decodePCX(buffer, width, height, palette) {
       }
     }
   }
-  //*/
 
   return out;
+}
+
+function implementation2(buffer, pcxWidth, pcxHeight, encoding, bpl, palette) {
+  const unknownVariable = encoding == 1 ? bpl : 0;
+  const out = Buffer.alloc(pcxWidth * pcxHeight * 4);
+
+  const p = Buffer.alloc(pcxWidth * pcxHeight);
+  let i = 0,
+    j = 0,
+    k = 0,
+    w = pcxWidth;
+  while (j < p.length) {
+    let n = 1;
+    let d = buffer[i];
+    if (i < buffer.length - 1) {
+      i++;
+    }
+    if (d >= 0xc0) {
+      n = d & 0x3f;
+      d = buffer[i];
+      if (i < buffer.length - 1) {
+        i++;
+      }
+    }
+    while (n > 0) {
+      if (k < w && j < out.length) {
+        p[j] = d;
+        j++;
+      }
+      k++;
+      if (k === unknownVariable) {
+        k = 0;
+        n = 1;
+      }
+      n--;
+    }
+  }
+
+  for (let ii = 0, aa = 0; ii < p.length; ii++, aa += 4) {
+    const paletteColorIndex = p[ii] * 4;
+
+    let red = palette[paletteColorIndex + 0];
+    let green = palette[paletteColorIndex + 1];
+    let blue = palette[paletteColorIndex + 2];
+    let alpha = palette[paletteColorIndex + 3];
+
+    // The first color is always transparent
+    if (paletteColorIndex === 0) {
+      red = 0;
+      green = 0;
+      blue = 0;
+      alpha = 0;
+    }
+
+    out[aa + 0] = red;
+    out[aa + 1] = green;
+    out[aa + 2] = blue;
+    out[aa + 3] = alpha;
+  }
+
+  return out;
+}
+
+export default function decodePCX(buffer, width, height, palette) {
+  let offset = 0;
+
+  const id = buffer.readUInt8(offset);
+  offset += 1;
+
+  const version = buffer.readUInt8(offset);
+  offset += 1;
+
+  const encoding = buffer.readUInt8(offset);
+  offset += 1;
+
+  const bitPerPixel = buffer.readUInt8(offset);
+  if (bitPerPixel != 8) {
+    throw new Error(`Invalid PCX, bitPerPixel: ${bitPerPixel}`);
+  }
+  offset += 1;
+
+  const x = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  const y = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  let pcxWidth = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  let pcxHeight = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  const hres = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  const vres = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  const colorMap = []; // 16 colors rgb
+  for (let i = 0; i < 16; i++) {
+    var c = [];
+    c[0] = buffer.readUInt8(offset);
+    offset += 1;
+    c[1] = buffer.readUInt8(offset);
+    offset += 1;
+    c[2] = buffer.readUInt8(offset);
+    offset += 1;
+    colorMap.push(c);
+  }
+
+  const reserved = buffer.readUInt8(offset);
+  offset += 1;
+
+  const nbPlanes = buffer.readUInt8(offset);
+  offset += 1;
+
+  const bpl = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  const paletteInfo = buffer.readUInt16LE(offset);
+  offset += 2;
+
+  const out = Buffer.alloc(pcxWidth * pcxHeight * 4);
+
+  offset = 128;
+  const imageData = buffer.subarray(offset);
+
+  return implementation2(imageData, pcxWidth, pcxHeight, encoding, bpl, palette);
+  //return implementation1(imageData, pcxWidth, pcxHeight, palette);
 }
