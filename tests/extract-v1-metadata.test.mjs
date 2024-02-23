3cf5e131af019bbe7ac7902c282fdf366e544e4d
neolao <contact@neolao.com>
2024-02-23 16:26:43 +0100

    first test

---
+++ tests/extract-v1-metadata.test.mjs
@@ -0,0 +1,16 @@
+import test from "node:test";
+import assert from "node:assert";
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname } from "node:path";
+import extract from "../index.mjs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+test("Extract v1 metadata", () => {
+  const buffer = readFileSync(`${__dirname}/files/cvsryu-v1.sff`);
+  const data = extract(buffer, { sprites: false, palettes: false });
+
+  assert.strictEqual(data.version, "1.0.1.0");
+});
